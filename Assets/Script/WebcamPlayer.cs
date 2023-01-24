7e5a762f448c6d96f76d09f6108c841d02339a14
Hugo SIMON <h.simon@ludus-academie.com>
2023-01-24 17:27:22 +0100

    Envoie toute la qualité MAIS
    Permet de tout envoyer mais pas vraiment, il faudrait découper tout les bytes par 65536 avant de les envoyer et les récup

--- Assets/Script/WebcamPlayer.cs
+++ Assets/Script/WebcamPlayer.cs
@@ -13,8 +13,8 @@ public class WebcamPlayer : NetworkBehaviour
 {
     private GameObject _webcamRawImage;
     public int webcamIndex = 0;
-    
-    private Texture2D _textureToSend;
+
+    public byte[] bytes;
 
     private void Start()
     {
@@ -29,23 +29,24 @@ public class WebcamPlayer : NetworkBehaviour
         component.material.mainTexture = laWebcam;
 
         _webcamRawImage.GetComponent<RawImage>().texture = laWebcam;
-        _textureToSend = new Texture2D(laWebcam.width, laWebcam.height);
 
         laWebcam.Play();
 
         ParentServerRpc();
+        
+        Array.Resize(ref bytes, laWebcam.width * laWebcam.height * 4);
     }
     
     [ServerRpc]
     private void ParentServerRpc()
     {
         transform.SetParent(GameObject.Find("Canvas").transform);
-        Debug.Log("Parent : " + transform.parent.name);
+        //Debug.Log("Parent : " + transform.parent.name);
 
         for (int i = transform.parent.childCount - 1; i >= 0; i--)
         {
             transform.parent.GetChild(i).GetComponent<WebcamPlayer>().PositionAleatoireClientRpc();
-            Debug.Log("Child no " + i + " : " + transform.parent.GetChild(i).name);
+            //Debug.Log("Child no " + i + " : " + transform.parent.GetChild(i).name);
         }
     }
 
@@ -63,34 +64,41 @@ public class WebcamPlayer : NetworkBehaviour
         
         transform.localPosition = new Vector3(Random.Range(-810, 810), Random.Range(-440, 440), 0);
         
-        Debug.Log("ClientRpc position");
+        //Debug.Log("ClientRpc position");
     }
 
     private void Update()
     {
         if (IsOwner)
         {
+            Texture2D _textureToSend = new Texture2D(_webcamRawImage.GetComponent<RawImage>().texture.width, _webcamRawImage.GetComponent<RawImage>().texture.height);
             _textureToSend.SetPixels(((WebCamTexture) _webcamRawImage.GetComponent<RawImage>().texture).GetPixels());
             _textureToSend.Apply();
             
-            byte[] bytes = _textureToSend.EncodeToPNG();
-            //Debug.Log("bytes : " + bytes.Length);
+            Array.Clear(bytes, 0, bytes.Length);
+            bytes = _textureToSend.EncodeToPNG();
+            Debug.Log("bytes : " + bytes.Length);
+            
+            
             
             TextureServerRpc(bytes, (int)NetworkManager.Singleton.LocalClientId);
         }
     }
     
     [ServerRpc]
-    private void TextureServerRpc(byte[] bytes, int clientId)
+    private void TextureServerRpc(byte[] bytesTexture, int clientId)
     {
-        TextureClientRpc(bytes, clientId);
+        Debug.Log("ServerRpc texture " + bytesTexture.Length);
+        //TextureClientRpc(bytesTexture, clientId);
     }
     
     [ClientRpc]
-    private void TextureClientRpc(byte[] bytes, int clientId)
+    private void TextureClientRpc(byte[] bytesLaTexture, int clientId)
     {
         Texture2D texture = new Texture2D(1, 1);
-        texture.LoadImage(bytes);
+        texture.LoadImage(bytesLaTexture);
+        
+        Debug.Log("texture : " + texture.width + "x" + texture.height);
         
         GameObject.Find("TestTexture").GetComponent<RawImage>().texture = texture;
     }
