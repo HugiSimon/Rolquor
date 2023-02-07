88fddfdab105e0f023c3f2e7af54998839a4e90f
Hugo SIMON <h.simon@ludus-academie.com>
2023-02-07 15:01:39 +0100

    Zoom map
    On peut zoomer sur la map quand le curseur est dessus

--- Assets/Script/InteractiveMap.cs
+++ Assets/Script/InteractiveMap.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class InteractiveMap : MonoBehaviour
 {
+    private float _actualScale = 1;
+    
+    public float zoom = 1;
+    [SerializeField] private CursonOn curseur;
+    
     void Start()
     {
         adaptScale();
     }
+    
+    void Update()
+    {
+        if (curseur.LeCurseur())
+        {
+            OnScroll();
+        }
+    }
 
     public void adaptScale()
     {
+        //Adaptation du groupe Map
         Transform enfantImage = transform.GetChild(0);
         GetComponent<RectTransform>().sizeDelta = new Vector2(enfantImage.GetComponent<RectTransform>().rect.width, enfantImage.GetComponent<RectTransform>().rect.height);
         
+        //Adaptation de la carte (image)
         GameObject parent = transform.parent.gameObject;
         float ratioParent = parent.GetComponent<RectTransform>().rect.width / parent.GetComponent<RectTransform>().rect.height;
         float ratioChild = GetComponent<RectTransform>().rect.width / GetComponent<RectTransform>().rect.height;
@@ -26,5 +41,22 @@ public class InteractiveMap : MonoBehaviour
         {
             GetComponent<RectTransform>().transform.localScale = new Vector3(parent.GetComponent<RectTransform>().rect.height / GetComponent<RectTransform>().rect.height, parent.GetComponent<RectTransform>().rect.height / GetComponent<RectTransform>().rect.height, 1);
         }
+        _actualScale = GetComponent<RectTransform>().localScale.x;
+    }
+    
+    // Quand on scroll sur la carte (image) on zoom
+    public void OnScroll()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll > 0f)
+        {
+            zoom += 0.1f;
+        }
+        else if (scroll < 0f)
+        {
+            zoom -= 0.1f;
+        }
+        zoom = Mathf.Clamp(zoom, 1, 10);
+        GetComponent<RectTransform>().transform.localScale = new Vector3(_actualScale * zoom, _actualScale * zoom, 1);
     }
 }
